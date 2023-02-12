3b681d12b216f68b218bfc4beeda61aa45c63a60
Steve Mattar <Steve.Mattar@skai.io>
2023-02-12 15:26:59 +0200

    chore: change name

--- screens/GameOverMenu.py
+++ screens/GameOverMenu.py
@@ -6,7 +6,7 @@ class GameOverMenu(object):
     def __init__(self):
         self.title = Text('GameOver', 32, (WINDOW_WIDTH - WINDOW_WIDTH * 0.5, WINDOW_HEIGHT - WINDOW_HEIGHT * 0.7), Colors.RED)
         self.to_restart_text = Text('Press ENTER to restart', 16, (WINDOW_WIDTH - WINDOW_WIDTH * 0.5, WINDOW_HEIGHT - WINDOW_HEIGHT * 0.3))
-        self.creator = Text('By Ahmad Waked', 10, (WINDOW_WIDTH - WINDOW_WIDTH * 0.1, WINDOW_HEIGHT - WINDOW_HEIGHT * 0.05))
+        self.creator = Text('By Steve Mattar', 10, (WINDOW_WIDTH - WINDOW_WIDTH * 0.1, WINDOW_HEIGHT - WINDOW_HEIGHT * 0.05))
 
     def render(self, core):
         self.title.render(core)
--- screens/MainMenu.py
+++ screens/MainMenu.py
@@ -6,7 +6,7 @@ class MainMenu(object):
     def __init__(self):
         self.title = Text('Cannon', 32, (WINDOW_WIDTH - WINDOW_WIDTH * 0.5, WINDOW_HEIGHT - WINDOW_HEIGHT * 0.7), Colors.YELLOW)
         self.to_start_text = Text('Press ENTER to start', 16, (WINDOW_WIDTH - WINDOW_WIDTH * 0.5, WINDOW_HEIGHT - WINDOW_HEIGHT * 0.3))
-        self.creator = Text('By Ahmad Waked', 10, (WINDOW_WIDTH - WINDOW_WIDTH * 0.1, WINDOW_HEIGHT - WINDOW_HEIGHT * 0.05))
+        self.creator = Text('By Steve Mattar', 10, (WINDOW_WIDTH - WINDOW_WIDTH * 0.1, WINDOW_HEIGHT - WINDOW_HEIGHT * 0.05))
 
     def render(self, core):
         self.title.render(core)
